5a215dbf57b6faec4a6051f72d70df1478ab07a9
mystborn <thefirewithin1017@gmail.com>
2019-09-09 00:06:21 -0500

    Better local iterator variable obfuscation

--- include/generic_iterators/rbtree_iterator.h
+++ include/generic_iterators/rbtree_iterator.h
@@ -1,55 +1,31 @@
 #ifndef GENERIC_DATA_STRUCTURES_GENERIC_ITERATORS_RED_BLACK_TREE_ITERATOR_H
 #define GENERIC_DATA_STRUCTURES_GENERIC_ITERATORS_RED_BLACK_TREE_ITERATOR_H
 
-typedef enum rbtree_travel_direction {
+typedef enum RbTreeTravelDirection {
     RBTREE_TRAVEL_LEFT,
     RBTREE_TRAVEL_RIGHT,
     RBTREE_TRAVEL_UP
-} rbtree_travel_direction;
+} RbTreeTravelDirection;
 
 /*
-    This iterator is far more complicated than the others, so I'll try and
-    describe what's happening step by step.
+    This algorithm is a specialized version of the one described here:
+    https://www.geeksforgeeks.org/inorder-non-threaded-binary-tree-traversal-without-recursion-or-stack/
 
-    0.  This process needs a couple of variables, so we start a new scope to
-        avoid name collisions. 
+    It has to take some extra steps to make sure the user can still use
+    break and continue inside of the loop and get the expected result.
 
-    1.  The current node is set to the root of the tree.
-    2.  While the node is not NULL, it iterates. This is important because
-        it will stop the iteration early if the root is NULL.
-    3.  Starting with left, it performs one of the iteration steps.
-        - Left: This step will only happen once. It goes as far left as possible,
-                sets the key and value to the nodes contents, then chooses the
-                next diection.
-        -Right: This step will only be reached if there was a right node. If
-                so, it moves to the right node, then travels all the way to
-                left. It sets the key and value, then chooses a direction.
-                Because this step also moves left, the left direction
-                will not be chosen again.
-        - Up:   First this step makes sure we're not finished by checking to
-                see if the parent node is not NULL. If it is, we've reached
-                the root node from the right and have finished the iteration.
-                Then, as long as we're coming from the right, the parent node
-                will have already been processed, so we just go up a node.
-                Finally, we're coming from the left. When this happens,
-                the parent has not been processed, so we set the key and value
-                and chooses the next direction.
-
-    4.  Choosing a direction: Each step ends with this. All this does is check
-        if there is any nodes to the right that need to be processed, and if so
-        the next step will move right, otherwise we'll move up the tree.
-
-    The macro is set up so that if a user wants to exit the iteration early,
-    they can still use break.
+    The local variables are also somewhat obfuscated to avoid potentiall 
+    name collisions, but due to how C shadows variables, it is still
+    possible to nest iter statements.
 */
 
 #define rbtree_iter_start(tree_type, tree, out_key, out_value) \
     { \
-        rbtree_travel_direction direction = RBTREE_TRAVEL_LEFT; \
+        RbTreeTravelDirection rb_travel_direction = RBTREE_TRAVEL_LEFT; \
         tree_type ## Node* rbtree_inorder_iter_node = (tree)->root; \
  \
         while(rbtree_inorder_iter_node != NULL) { \
-            switch(direction) { \
+            switch(rb_travel_direction) { \
                 case RBTREE_TRAVEL_LEFT: \
                 { \
                     while(rbtree_inorder_iter_node->left != NULL) \
@@ -58,7 +34,7 @@ typedef enum rbtree_travel_direction {
                     out_key = rbtree_inorder_iter_node->key; \
                     out_value = rbtree_inorder_iter_node->value; \
  \
-                    direction = rbtree_inorder_iter_node->right == NULL ? RBTREE_TRAVEL_UP : RBTREE_TRAVEL_RIGHT; \
+                    rb_travel_direction = rbtree_inorder_iter_node->right == NULL ? RBTREE_TRAVEL_UP : RBTREE_TRAVEL_RIGHT; \
                     break; \
                 } \
                 case RBTREE_TRAVEL_RIGHT: \
@@ -70,7 +46,7 @@ typedef enum rbtree_travel_direction {
                     out_key = rbtree_inorder_iter_node->key; \
                     out_value = rbtree_inorder_iter_node->value; \
  \
-                    direction = rbtree_inorder_iter_node->right == NULL ? RBTREE_TRAVEL_UP : RBTREE_TRAVEL_RIGHT; \
+                    rb_travel_direction = rbtree_inorder_iter_node->right == NULL ? RBTREE_TRAVEL_UP : RBTREE_TRAVEL_RIGHT; \
                     break; \
                 } \
                 case RBTREE_TRAVEL_UP: \
@@ -86,18 +62,18 @@ typedef enum rbtree_travel_direction {
                     out_key = rbtree_inorder_iter_node->key; \
                     out_value = rbtree_inorder_iter_node->value; \
  \
-                    direction = rbtree_inorder_iter_node->right == NULL ? RBTREE_TRAVEL_UP : RBTREE_TRAVEL_RIGHT; \
+                    rb_travel_direction = rbtree_inorder_iter_node->right == NULL ? RBTREE_TRAVEL_UP : RBTREE_TRAVEL_RIGHT; \
                     break; \
                 } \
             }
 
 #define rbtree_iter_keys_start(tree_type, tree, out_key) \
     { \
-        rbtree_travel_direction direction = RBTREE_TRAVEL_LEFT; \
+        RbTreeTravelDirection rb_travel_direction = RBTREE_TRAVEL_LEFT; \
         tree_type ## Node* rbtree_inorder_iter_node = (tree)->root; \
  \
         while(rbtree_inorder_iter_node != NULL) { \
-            switch(direction) { \
+            switch(rb_travel_direction) { \
                 case RBTREE_TRAVEL_LEFT: \
                 { \
                     while(rbtree_inorder_iter_node->left != NULL) \
@@ -105,7 +81,7 @@ typedef enum rbtree_travel_direction {
  \
                     out_key = rbtree_inorder_iter_node->key; \
  \
-                    direction = rbtree_inorder_iter_node->right == NULL ? RBTREE_TRAVEL_UP : RBTREE_TRAVEL_RIGHT; \
+                    rb_travel_direction = rbtree_inorder_iter_node->right == NULL ? RBTREE_TRAVEL_UP : RBTREE_TRAVEL_RIGHT; \
                     break; \
                 } \
                 case RBTREE_TRAVEL_RIGHT: \
@@ -116,7 +92,7 @@ typedef enum rbtree_travel_direction {
  \
                     out_key = rbtree_inorder_iter_node->key; \
  \
-                    direction = rbtree_inorder_iter_node->right == NULL ? RBTREE_TRAVEL_UP : RBTREE_TRAVEL_RIGHT; \
+                    rb_travel_direction = rbtree_inorder_iter_node->right == NULL ? RBTREE_TRAVEL_UP : RBTREE_TRAVEL_RIGHT; \
                     break; \
                 } \
                 case RBTREE_TRAVEL_UP: \
@@ -131,18 +107,18 @@ typedef enum rbtree_travel_direction {
                     rbtree_inorder_iter_node = rbtree_inorder_iter_node->parent; \
                     out_key = rbtree_inorder_iter_node->key; \
  \
-                    direction = rbtree_inorder_iter_node->right == NULL ? RBTREE_TRAVEL_UP : RBTREE_TRAVEL_RIGHT; \
+                    rb_travel_direction = rbtree_inorder_iter_node->right == NULL ? RBTREE_TRAVEL_UP : RBTREE_TRAVEL_RIGHT; \
                     break; \
                 } \
             }
 
 #define rbtree_iter_values_start(tree_type, tree, out_value) \
     { \
-        rbtree_travel_direction direction = RBTREE_TRAVEL_LEFT; \
+        RbTreeTravelDirection rb_travel_direction = RBTREE_TRAVEL_LEFT; \
         tree_type ## Node* rbtree_inorder_iter_node = (tree)->root; \
  \
         while(rbtree_inorder_iter_node != NULL) { \
-            switch(direction) { \
+            switch(rb_travel_direction) { \
                 case RBTREE_TRAVEL_LEFT: \
                 { \
                     while(rbtree_inorder_iter_node->left != NULL) \
@@ -150,7 +126,7 @@ typedef enum rbtree_travel_direction {
  \
                     out_value = rbtree_inorder_iter_node->value; \
  \
-                    direction = rbtree_inorder_iter_node->right == NULL ? RBTREE_TRAVEL_UP : RBTREE_TRAVEL_RIGHT; \
+                    rb_travel_direction = rbtree_inorder_iter_node->right == NULL ? RBTREE_TRAVEL_UP : RBTREE_TRAVEL_RIGHT; \
                     break; \
                 } \
                 case RBTREE_TRAVEL_RIGHT: \
@@ -161,7 +137,7 @@ typedef enum rbtree_travel_direction {
  \
                     out_value = rbtree_inorder_iter_node->value; \
  \
-                    direction = rbtree_inorder_iter_node->right == NULL ? RBTREE_TRAVEL_UP : RBTREE_TRAVEL_RIGHT; \
+                    rb_travel_direction = rbtree_inorder_iter_node->right == NULL ? RBTREE_TRAVEL_UP : RBTREE_TRAVEL_RIGHT; \
                     break; \
                 } \
                 case RBTREE_TRAVEL_UP: \
@@ -176,7 +152,7 @@ typedef enum rbtree_travel_direction {
                     rbtree_inorder_iter_node = rbtree_inorder_iter_node->parent; \
                     out_value = rbtree_inorder_iter_node->value; \
  \
-                    direction = rbtree_inorder_iter_node->right == NULL ? RBTREE_TRAVEL_UP : RBTREE_TRAVEL_RIGHT; \
+                    rb_travel_direction = rbtree_inorder_iter_node->right == NULL ? RBTREE_TRAVEL_UP : RBTREE_TRAVEL_RIGHT; \
                     break; \
                 } \
             }
