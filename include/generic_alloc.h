5b0822b6fca81adf536cde56139cb033774d80fc
Chris Kramer <thefirewithin1017@gmail.com>
2024-01-29 22:51:00 -0600

    Adding custom allocation functions

---
+++ include/generic_alloc.h
@@ -0,0 +1,28 @@
+#ifndef GENERIC_DATA_STRUCTURES_GENERIC_ALLOC_H
+#define GENERIC_DATA_STRUCTURES_GENERIC_ALLOC_H
+
+#ifndef gds_malloc
+#define gds_malloc malloc
+#endif
+
+#ifndef gds_realloc
+#define gds_realloc realloc
+#endif
+
+#ifndef gds_calloc
+#define gds_callod calloc
+#endif
+
+#ifndef gds_free
+#define gds_free free
+#endif
+
+#ifndef gds_assert_arg
+#define gds_assert_arg assert
+#endif
+
+#ifndef gds_assert_bounds
+#define gds_assert_bounds assert
+#endif
+
+#endif
--- include/generic_list.h
+++ include/generic_list.h
@@ -6,6 +6,7 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
+#include "generic_alloc.h"
 
 // Todo: 
 // bool add_range(type_name* list, value_type* values, unsigned int count);
@@ -28,22 +29,22 @@
     \
     static inline unsigned int function_prefix ## _count(type_name* list) { return list->count; } \
     \
-    static inline void function_prefix ## _free_resources(type_name* list) { free(list->buffer); } \
+    static inline void function_prefix ## _free_resources(type_name* list) { gds_free(list->buffer); } \
     \
-    static inline void function_prefix ## _free(type_name* list) { free(list->buffer); free(list); } \
+    static inline void function_prefix ## _free(type_name* list) { gds_free(list->buffer); gds_free(list); } \
     \
     static inline value_type function_prefix ## _get(type_name* list, unsigned int index) { \
-        assert(index < list->count); \
+        gds_assert_bounds(index < list->count); \
         return list->buffer[index]; \
     } \
     \
     static inline value_type* function_prefix ## _get_ref(type_name* list, unsigned int index) { \
-        assert(index < list->count); \
+        gds_assert_bounds(index < list->count); \
         return list->buffer + index; \
     } \
     \
     static inline void function_prefix ## _set(type_name* list, unsigned int index, value_type value) { \
-        assert(index <= list->count); \
+        gds_assert_bounds(index <= list->count); \
         if(index == list->count) \
             function_prefix ## _add(list, value); \
         else \
@@ -51,7 +52,7 @@
     } \
     \
     static inline void function_prefix ## _remove(type_name* list, unsigned int index) { \
-        assert(index < list->count); \
+        gds_assert_bounds(index < list->count); \
         if(index == --list->count) \
             return; \
         else \
@@ -59,28 +60,28 @@
     } \
     \
     static inline value_type function_prefix ## _peek(type_name* list) { \
-        assert(list->count); \
+        gds_assert_bounds(list->count); \
         return list->buffer[list->count - 1]; \
     } \
     \
     static inline value_type function_prefix ## _pop(type_name* list) {  \
-        assert(list->count); \
+        gds_assert_bounds(list->count); \
         return list->buffer[--list->count]; \
     } \
     \
     static inline void function_prefix ## _shrink_to_fit(type_name* list) { \
-        list->buffer = realloc(list->buffer, (list->count == 0 ? 1 : list->count) * sizeof(value_type)); \
+        list->buffer = gds_realloc(list->buffer, (list->count == 0 ? 1 : list->count) * sizeof(value_type)); \
         list->capacity = list->count == 0 ? 1 : list->count; \
     } \
 
 
 #define LIST_DEFINE_C(type_name, function_prefix, value_type) \
     type_name* function_prefix ## _create(void) { \
-        type_name* list = malloc(sizeof(type_name)); \
+        type_name* list = gds_malloc(sizeof(type_name)); \
         if(!list) \
             return list; \
         if(!function_prefix ## _init(list)) { \
-            free(list); \
+            gds_free(list); \
             return NULL; \
         } \
         return list; \
@@ -89,20 +90,20 @@
     bool function_prefix ## _init(type_name* list) { \
         list->capacity = 4; \
         list->count = 0; \
-        return (list->buffer = malloc(4 * sizeof(value_type))) != NULL; \
+        return (list->buffer = gds_malloc(4 * sizeof(value_type))) != NULL; \
     } \
     \
     bool function_prefix ## _init_capacity(type_name* list, unsigned int capacity) { \
-        assert(capacity); \
+        gds_assert_arg(capacity); \
         list->capacity = capacity; \
         list->count = 0; \
-        return (list->buffer = malloc(capacity * sizeof(value_type))) != NULL; \
+        return (list->buffer = gds_malloc(capacity * sizeof(value_type))) != NULL; \
     } \
     \
     bool function_prefix ## _add(type_name* list, value_type value) { \
         if(list->count == list->capacity) { \
             list->capacity *= 2; \
-            value_type* buffer = realloc(list->buffer, list->capacity * sizeof(value_type)); \
+            value_type* buffer = gds_realloc(list->buffer, list->capacity * sizeof(value_type)); \
             if(!buffer) \
                 return false; \
             list->buffer = buffer; \
@@ -117,7 +118,7 @@
             return function_prefix ## _add(list, value); \
         if(list->count == list->capacity) { \
             list->capacity *= 2; \
-            value_type* buffer = realloc(list->buffer, list->capacity * sizeof(value_type)); \
+            value_type* buffer = gds_realloc(list->buffer, list->capacity * sizeof(value_type)); \
             if(!buffer) \
                 return false; \
             list->buffer = buffer; \
